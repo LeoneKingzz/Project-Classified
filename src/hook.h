8dfe2105d8b6a188c617127b3958828ce19457e3
LeoneKingzz <167722980+LeoneKingzz@users.noreply.github.com>
2024-11-17 13:43:46 +0000

    olad

--- src/hook.h
+++ src/hook.h
@@ -412,35 +412,7 @@ namespace hooks
 
 	};
 
-	class InputEventHandler : public RE::BSTEventSink<RE::InputEvent*>
-	{
-	public:
-
-		static InputEventHandler*	GetSingleton();
-
-		virtual EventResult			ProcessEvent(RE::InputEvent* const* a_event, RE::BSTEventSource<RE::InputEvent*>* a_eventSource) override;
-
-		static void SinkEventHandlers();
-
-	private:
-		enum : uint32_t
-		{
-			kInvalid = static_cast<uint32_t>(-1),
-			kKeyboardOffset = 0,
-			kMouseOffset = 256,
-			kGamepadOffset = 266
-		};
-
-		InputEventHandler() = default;
-		InputEventHandler(const InputEventHandler&) = delete;
-		InputEventHandler(InputEventHandler&&) = delete;
-		virtual ~InputEventHandler() = default;
-
-		InputEventHandler& operator=(const InputEventHandler&) = delete;
-		InputEventHandler& operator=(InputEventHandler&&) = delete;
-
-		std::uint32_t GetGamepadIndex(RE::BSWin32GamepadDevice::Key a_key);
-	};
+	
 
 
 	class util
