c40ed595a5ce733e9c4f0484749ae3d3840f0fa1
LeoneKingzz <167722980+LeoneKingzz@users.noreply.github.com>
2024-11-22 20:03:53 +0000

    Update GeneralFunctions.cpp

--- src/GeneralFunctions.cpp
+++ src/GeneralFunctions.cpp
@@ -136,53 +136,6 @@ namespace GFunc_Space{
 		std::get<3>(data) = d;
 	}
 
-	void GFunc::Process_Updates(RE::Actor *a_actor, GFunc_Space::Time::time_point time_now){
-		uniqueLocker lock(mtx_Timer);
-		for (auto it = _Timer.begin(); it != _Timer.end(); ++it){
-			if (it->first == a_actor){
-				if (!it->second.empty()){
-					for (auto data : it->second){
-						auto update = std::get<0>(data);
-						if (update){
-							auto time_initial = std::get<1>(data);
-							auto time_required = std::get<2>(data);
-							if (duration_cast<ms>(time_now - time_initial).count() >= time_required.count()){
-								auto function = std::get<3>(data);
-								auto H = RE::TESDataHandler::GetSingleton();
-								switch (hash(function.c_str(), function.size())){
-								case "TATripleThreat_Update"_h:
-									a_actor->SetGraphVariableBool("bLDP_TripleThreat_Faction", false);
-									break;
-
-								case "DeathTimeTravel_Update"_h:
-									GFunc_Space::GFunc::playSound(a_actor, (H->LookupForm<RE::BGSSoundDescriptorForm>(0x802, "LeoneDragonProject.esp"))); // ks_NPCDragonKillMove
-
-								case "DeathDefault_Update"_h:
-								case "DeathAgony_Update"_h:
-								case "DeathInjured_Update"_h:
-								case "DeathBleedout_Update"_h:
-									a_actor->NotifyAnimationGraph("Ragdoll");
-									break;
-
-								default:
-									break;
-								}
-								std::vector<std::tuple<bool, GFunc_Space::Time::time_point, GFunc_Space::ms, std::string>>::iterator position = std::find(it->second.begin(), it->second.end(), data);
-								if (position != it->second.end())
-								{
-									it->second.erase(position);
-								}
-							}
-						}
-					}
-				}else{
-					_Timer.erase(it);
-				}
-				break;
-			}
-		}
-	}
-
 	void GFunc::Re_EquipAll(RE::Actor *a_actor)
 	{
 		uniqueLocker lock(mtx_Inventory);
