fe0e2d9ce1e8f5909025ca2050b5825e00a0ec64
LeoneKingzz <167722980+LeoneKingzz@users.noreply.github.com>
2024-11-25 04:09:44 +0000

    Update DovahAI.h

--- src/DovahAI.h
+++ src/DovahAI.h
@@ -39,6 +39,7 @@ namespace DovahAI_Space{
 		static bool IsUnSafeDragon(RE::Actor *a_actor);
 		static void DamageTarget(RE::Actor *a_actor, RE::Actor *enemy);
 		static void CalcLevelRank(RE::Actor *a_actor);
+		static void SetValuesDragon(RE::Actor *a_actor);
 		static void Physical_Impact(RE::Actor *a_actor, std::string a_spell, float p_force);
 		static void Random_TakeOffandDeath_Anims(RE::Actor *a_actor);
 		static void GetEquippedShout(RE::Actor *actor, bool SpellFire = false);
